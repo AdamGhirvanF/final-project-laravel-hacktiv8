e69c72ab40cd06ffa83f03715b94593e17759118
AdamGhirvanF <adam_ghirvan@yahoo.co.id>
2024-08-30 07:37:55 +0700

    Move category & product to unauthenticated

--- routes/api.php
+++ routes/api.php
@@ -12,9 +12,10 @@ Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 Route::get('login', [AuthController::class,'loginFirst'])->name('login');
 
+Route::apiResource('category', CategoryController::class);
+Route::apiResource('product', ProductController::class);
+
 Route::middleware('auth:api')->group(function () {
-    Route::apiResource('category', CategoryController::class);
-    Route::apiResource('product', ProductController::class);
     Route::apiResource('order', OrderController::class);
     Route::get('report-order', [OrderController::class, 'report']);
 });
